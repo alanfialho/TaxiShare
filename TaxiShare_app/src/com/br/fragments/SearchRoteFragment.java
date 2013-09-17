123fa29d30200a2e3d6d43bd3e217c26bef95e69
Manjubex <brunosp15@gmail.com>
2013-09-16 23:13:53 -0300

    adapter de rota e tela com a lista das rotas

--- TaxiShare_app/src/com/br/fragments/SearchRoteFragment.java
+++ TaxiShare_app/src/com/br/fragments/SearchRoteFragment.java
@@ -2,168 +2,56 @@ package com.br.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+
 import com.br.activitys.R;
+import com.br.adapter.RoteAdapter;
 import com.br.resources.Utils;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 public class SearchRoteFragment extends Fragment {
 
-	// Google Map
-	private GoogleMap googleMap;
 	private static View view;
+	private ListView RoteList;
+	private Button searchButton;
+
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.rote_search, container, false);
+		view = inflater.inflate(R.layout.rote_search, container, false);
 
-		
-		
-		
-		if (view != null) {
-	        ViewGroup parent = (ViewGroup) view.getParent();
-	        if (parent != null)
-	            parent.removeView(view);
-	    }
-	    try {
-	    	view = inflater.inflate(R.layout.rote_search, container, false);
-	    } catch (InflateException e) {
-	        /* map is already there, just return view as it is */
-	    }
-		
-		
-
-		try {
-			// Loading map
-			initilizeMap(view);
-
-			// Changing map type
-			googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-			// googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-			// googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-			// googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
-			// googleMap.setMapType(GoogleMap.MAP_TYPE_NONE);
-
-			// Showing / hiding your current location
-			googleMap.setMyLocationEnabled(true);
-
-			// Enable / Disable zooming controls
-			googleMap.getUiSettings().setZoomControlsEnabled(false);
-
-			// Enable / Disable my location button
-			googleMap.getUiSettings().setMyLocationButtonEnabled(true);
-
-			// Enable / Disable Compass icon
-			googleMap.getUiSettings().setCompassEnabled(true);
-
-			// Enable / Disable Rotate gesture
-			googleMap.getUiSettings().setRotateGesturesEnabled(true);
-
-			// Enable / Disable zooming functionality
-			googleMap.getUiSettings().setZoomGesturesEnabled(true);
-
-			double latitude = 17.385044;
-			double longitude = 78.486671;
-
-			// lets place some 10 random markers
-			for (int i = 0; i < 10; i++) {
-				// random latitude and logitude
-				double[] randomLocation = createRandLocation(latitude,
-						longitude);
-
-				// Adding a marker
-				MarkerOptions marker = new MarkerOptions().position(
-						new LatLng(randomLocation[0], randomLocation[1]))
-						.title("Hello Maps " + i);
-
-				Log.e("Random", "> " + randomLocation[0] + ", "
-						+ randomLocation[1]);
-
-				// changing marker color
-				if (i == 0)
-					marker.icon(BitmapDescriptorFactory
-							.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
-				if (i == 1)
-					marker.icon(BitmapDescriptorFactory
-							.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
-				if (i == 2)
-					marker.icon(BitmapDescriptorFactory
-							.defaultMarker(BitmapDescriptorFactory.HUE_CYAN));
-				if (i == 3)
-					marker.icon(BitmapDescriptorFactory
-							.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
-				if (i == 4)
-					marker.icon(BitmapDescriptorFactory
-							.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
-				if (i == 5)
-					marker.icon(BitmapDescriptorFactory
-							.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE));
-				if (i == 6)
-					marker.icon(BitmapDescriptorFactory
-							.defaultMarker(BitmapDescriptorFactory.HUE_RED));
-				if (i == 7)
-					marker.icon(BitmapDescriptorFactory
-							.defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
-				if (i == 8)
-					marker.icon(BitmapDescriptorFactory
-							.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET));
-				if (i == 9)
-					marker.icon(BitmapDescriptorFactory
-							.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW));
-
-				googleMap.addMarker(marker);
-
-				// Move the camera to last position with a zoom level
-				if (i == 9) {
-					CameraPosition cameraPosition = new CameraPosition.Builder()
-					.target(new LatLng(randomLocation[0],
-							randomLocation[1])).zoom(15).build();
-
-					googleMap.animateCamera(CameraUpdateFactory
-							.newCameraPosition(cameraPosition));
-				}
+		searchButton = (Button) view.findViewById(R.id.btnListarRotas);
+		searchButton.setOnClickListener(new View.OnClickListener() {			
+			public void onClick(View view) {
+				fillSearchList();
 			}
+		});
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 		return view;
 	}
 
-
-
-	/**
-	 * function to load map If map is not created it will create it for you
-	 * */
-	private void initilizeMap(View rootView) {
-		if (googleMap == null) {
-			googleMap = ((MapFragment) getFragmentManager().findFragmentById(
-					R.id.novoMapa)).getMap();
-
-			// check if map is created successfully or not
-			if (googleMap == null) {
-				Utils.gerarToast(rootView.getContext(), "Nao sei");
-			}
+	/* The click listner for ListView in the navigation drawer */
+	private class RoteListItemClickListener implements ListView.OnItemClickListener {
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+			Utils.gerarToast(view.getContext(),  "Particiapando da rota");
 		}
 	}
 
-	/*
-	 * creating random postion around a location for testing purpose only
-	 */
-	private double[] createRandLocation(double latitude, double longitude) {
+	private void fillSearchList(){
+		String[] rotes = getResources().getStringArray(R.array.enderecos) ;
 
-		return new double[] { latitude + ((Math.random() - 0.5) / 500),
-				longitude + ((Math.random() - 0.5) / 500),
-				150 + ((Math.random() - 0.5) * 10) };
+		RoteAdapter roteAdapter = new RoteAdapter(view.getContext(), rotes);
+
+		RoteList = (ListView) view.findViewById(R.id.listViewRoteList);
+		RoteList.setAdapter(roteAdapter);
+		RoteList.setOnItemClickListener(new RoteListItemClickListener());
+
 	}
+
 }
